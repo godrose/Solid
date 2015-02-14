72a0961e868b9a3e83df2710e75ff7827c8d5266
godrose <godrose@gmail.com>
2015-02-14 14:04:02 +0200

    Added Practices.IoC

---
+++ Solid.Practices.IoC/IIocContainer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Solid.Practices.IoC
+{
+    public interface IIocContainer
+    {
+        void RegisterTransient<TService, TImplementation>() where TImplementation : class, TService;
+        void RegisterTransient<TService>() where TService : class;
+        void RegisterTransient(Type serviceType, Type implementationType);
+        void RegisterSingleton<TService, TImplementation>() where TImplementation : class, TService;
+        void RegisterInstance<TService>(TService instance) where TService : class;
+    }
+}
---
+++ Solid.Practices.IoC/IServiceLocator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Solid.Practices.IoC
+{
+    public interface IServiceLocator
+    {
+        TService GetInstance<TService>(Type serviceType) where TService : class;
+        TService GetInstance<TService>() where TService : class;
+        object GetInstance(Type serviceType);
+        IEnumerable<object> GetAllInstances(Type serviceType);
+        void BuildUp(object instance);
+    }
+}
