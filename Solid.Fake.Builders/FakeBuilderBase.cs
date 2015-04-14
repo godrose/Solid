ff355ef53ea2b21204b08e7ab81c1589c57a6e08
godrose <godrose@gmail.com>
2015-04-14 16:49:45 +0300

    Added Fake.Builders project providing base class for creating fake builders

---
+++ Solid.Fake.Builders/FakeBuilderBase.cs
@@ -0,0 +1,18 @@
+﻿using Solid.Data.Fake.Core;
+using Solid.Fake.Moq;
+
+namespace Solid.Fake.Builders
+{
+    public abstract class FakeBuilderBase<TService> where TService : class
+    {
+        protected readonly IFake<TService> FakeService = FakeProvider.CreateFake<TService>();
+
+        protected abstract void SetupFake();
+
+        public TService GetService()
+        {
+            SetupFake();
+            return FakeService.Object;
+        }
+    }
+}
