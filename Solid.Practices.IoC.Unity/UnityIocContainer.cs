f0ada0367f66f2116da089e08c29de6781442ec2
godrose <godrose@gmail.com>
2015-04-14 20:34:18 +0300

    Added Unity implementation of IIocContainer

---
+++ Solid.Practices.IoC.Unity/UnityIocContainer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.Practices.Unity;
+
+namespace Solid.Practices.IoC.Unity
+{
+    class UnityIocContainer : IIocContainer
+    {
+        private readonly UnityContainer _container = new UnityContainer();
+
+        public void RegisterTransient<TService, TImplementation>() where TImplementation : class, TService
+        {
+            _container.RegisterType<TService, TImplementation>();
+        }
+
+        public void RegisterTransient<TService>() where TService : class
+        {
+            _container.RegisterType<TService>();
+        }
+
+        public void RegisterTransient(Type serviceType, Type implementationType)
+        {
+            _container.RegisterType(serviceType, implementationType);
+        }
+
+        public void RegisterSingleton<TService, TImplementation>() where TImplementation : class, TService
+        {
+            _container.RegisterType<TService, TImplementation>(new ContainerControlledLifetimeManager());
+        }
+
+        public void RegisterInstance<TService>(TService instance) where TService : class
+        {
+            _container.RegisterInstance(instance, new ContainerControlledLifetimeManager());
+        }
+
+        public TService Resolve<TService>() where TService : class
+        {
+            return _container.Resolve<TService>();
+        }
+    }
+}
