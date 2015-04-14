3869ce4108edb39bfe90f04dc4430abff9fccb98
godrose <godrose@gmail.com>
2015-04-14 19:47:22 +0300

    Added implementation of IFake for FakeItEasy

---
+++ Solid.Fake.FakeItEasy/Fake.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq.Expressions;
+using FakeItEasy;
+using Solid.Data.Fake.Core;
+
+namespace Solid.Fake.FakeItEasy
+{
+    public class Fake<TFaked> : IFake<TFaked> where TFaked : class
+    {
+        private readonly TFaked _fake = A.Fake<TFaked>();
+
+        public void VerifyCall(Expression<Action<TFaked>> expression)
+        {
+            A.CallTo(expression).MustHaveHappened();
+        }
+
+        public void VerifyNoCall(Expression<Action<TFaked>> expression)
+        {
+            A.CallTo(expression).MustNotHaveHappened();
+        }
+
+        public IFake<TFaked> SetupWithResult<TResult>(Expression<Func<TFaked, TResult>> expression, TResult result)
+        {
+            A.CallTo(() =>expression.Compile().Invoke(_fake)).Returns(result);
+            return this;
+        }
+
+        public IFake<TFaked> SetupWithException<TResult>(Expression<Func<TFaked, TResult>> expression, Exception exception)
+        {
+            A.CallTo(() => expression.Compile().Invoke(_fake)).Throws(exception);
+            return this;
+        }
+
+        public TFaked Object
+        {
+            get { return _fake; }
+        }
+    }
+}
---
+++ Solid.Fake.FakeItEasy/FakeFactory.cs
@@ -0,0 +1,12 @@
+﻿using Solid.Data.Fake.Core;
+
+namespace Solid.Fake.FakeItEasy
+{
+    public class FakeFactory : IFakeFactory
+    {
+        public IFake<TFaked> CreateFake<TFaked>() where TFaked : class
+        {
+            return new Fake<TFaked>();
+        }
+    }
+}
