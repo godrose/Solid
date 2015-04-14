9c3797a3bb23e46943631a3a1d1160497671b9ab
godrose <godrose@gmail.com>
2015-04-14 17:04:53 +0300

    Added base class for fake data agent - useful for creating fake data sources

---
+++ Solid.Fake.Builders/FakeDataAgentBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Solid.Fake.Builders
+{
+    public abstract class FakeDataAgentBase<TBuilder, TService>
+        where TService : class
+        where TBuilder : FakeBuilderBase<TService>
+    {
+        protected TService GetService(Func<TBuilder> createBuilder, Func<TBuilder, TBuilder> setupMiddleware)
+        {
+            var builder = createBuilder();
+            builder = setupMiddleware(builder);
+            return builder.GetService();
+        }
+    }
+}
