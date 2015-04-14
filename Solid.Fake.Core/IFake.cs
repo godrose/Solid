23e8191dc96922dc9f8c51f1ffb225521e8512c5
godrose <godrose@gmail.com>
2015-04-14 16:29:41 +0300

    Added Fake.Core project - supporting creating fake objects for tests and fake data services and agents

---
+++ Solid.Fake.Core/IFake.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Solid.Data.Fake.Core
+{
+    public interface IFake<TFaked> : IMock<TFaked> where TFaked: class
+    {
+        IFake<TFaked> SetupWithResult<TResult>(Expression<Func<TFaked, TResult>> expression, TResult result);
+        IFake<TFaked> SetupWithException<TResult>(Expression<Func<TFaked, TResult>> expression, Exception exception);
+        TFaked Object { get; }
+    }
+}
---
+++ Solid.Fake.Core/IMock.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Solid.Data.Fake.Core
+{
+    public interface IMock<TFaked> where TFaked : class
+    {
+        void VerifyCall(Expression<Action<TFaked>> expression);
+        void VerifyNoCall(Expression<Action<TFaked>> expression);
+    }
+}
