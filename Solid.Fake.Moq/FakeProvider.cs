64452391c99ccfeee5e3eca9e6c2ba7179baee92
godrose <godrose@gmail.com>
2015-04-14 16:43:41 +0300

    Added Fake.Core implementation for Moq

---
+++ Solid.Fake.Moq/FakeProvider.cs
@@ -0,0 +1,18 @@
+﻿using Moq;
+using Solid.Data.Fake.Core;
+
+namespace Solid.Fake.Moq
+{
+    public static class FakeProvider
+    {
+        public static IFake<TFaked> CreateFake<TFaked>() where TFaked : class
+        {
+            return new Fake<TFaked>(CreateMock<TFaked>());
+        }
+
+        private static Mock<TFaked> CreateMock<TFaked>() where TFaked : class
+        {
+            return new Mock<TFaked>(MockBehavior.Default);
+        }
+    }
+}
