d8990c8395d7b0db1b77f68ed9e0caf2247ba029
godrose <godrose@gmail.com>
2015-04-20 11:04:42 +0300

    Added Scheduling project with TaskScheduler

---
+++ Solid.Practices.Scheduling/AsyncProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Solid.Practices.Scheduling
+{
+    class AsyncProvider
+    {
+        public static Task RunAsync(Action action)
+        {
+            return Task.Factory.StartNew(action, new CancellationToken(), new TaskCreationOptions(),
+                TaskScheduler.Current);
+        }
+
+        public static Task RunAsync<T>(Func<T> method)
+        {
+            return Task.Factory.StartNew(method, new CancellationToken(), new TaskCreationOptions(),
+                TaskScheduler.Current);
+        }
+    }
+}
---
+++ Solid.Practices.Scheduling/TaskScheduler.cs
@@ -0,0 +1,12 @@
+﻿namespace Solid.Practices.Scheduling
+{
+    public static class TaskScheduler
+    {
+        private static System.Threading.Tasks.TaskScheduler _taskScheduler = System.Threading.Tasks.TaskScheduler.Default;
+        public static System.Threading.Tasks.TaskScheduler Current
+        {
+            get { return _taskScheduler; }
+            set { _taskScheduler = value; }
+        }
+    }
+}
