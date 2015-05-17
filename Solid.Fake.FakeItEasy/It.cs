6ffbdccb916c707c05a192628b904a91336bf20f
godrose <godrose@gmail.com>
2015-05-17 20:54:06 +0300

    Implemented It for FakeItEasy

---
+++ Solid.Fake.FakeItEasy/It.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq.Expressions;
+using FakeItEasy;
+
+namespace Solid.Fake.FakeItEasy
+{
+    public static class It
+    {
+        public static TValue IsAny<TValue>()
+        {
+            return A<TValue>.Ignored;
+        }
+
+        public static TValue Is<TValue>(Expression<Func<TValue, bool>> testExpression)
+        {
+            return A<TValue>.That.Matches(testExpression);
+        }
+    }
+}
