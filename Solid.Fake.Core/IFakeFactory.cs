306421108224883999b6e1912ddcd2a18703444d
godrose <godrose@gmail.com>
2015-05-22 13:18:35 +0300

    Moved Fake/Middleware/Tests assemblies into the Attest repo

--- Solid.Fake.Core/IFakeFactory.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Solid.Fake.Core
-{
-    public interface IFakeFactory
-    {
-        IFake<TFaked> CreateFake<TFaked>() where TFaked : class;
-        IMock<TFaked> CreateMock<TFaked>() where TFaked : class;
-    }
-}
--- Solid.Middleware/IMiddleware.cs
+++
@@ -1,9 +0,0 @@
-﻿using Solid.Practices.IoC;
-
-namespace Solid.Middleware
-{
-    public interface IMiddleware
-    {
-        void Apply(IIocContainer iocContainer);
-    }
-}
