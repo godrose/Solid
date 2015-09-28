3010b9107a0b2799e9ab3eaa1c5f23f621a7d58b
godrose <godrose@gmail.com>
2015-09-28 19:44:13 +0300

    Added comments to Solid.Practices.IoC package

--- Solid.Practices.IoC/IIocContainer.cs
+++ Solid.Practices.IoC/IIocContainer.cs
@@ -2,17 +2,51 @@
 
 namespace Solid.Practices.IoC
 {
+    /// <summary>
+    /// Represents Inversion-Of-Control container
+    /// </summary>
     public interface IIocContainer : IIocContainerRegistrator, IIocContainerResolver
     {      
           
     }
 
+    /// <summary>
+    /// Allows registering dependencies into the IoC container
+    /// </summary>
     public interface IIocContainerRegistrator
     {
+        /// <summary>
+        /// Registers dependency in a transient lifetime style
+        /// </summary>
+        /// <typeparam name="TService">Type of dependency declaration</typeparam>
+        /// <typeparam name="TImplementation">Type of dependency implementation</typeparam>
         void RegisterTransient<TService, TImplementation>() where TImplementation : class, TService;
+
+        /// <summary>
+        /// Registers dependency in a transient lifetime style
+        /// </summary>
+        /// <typeparam name="TService">Type of dependency</typeparam>
         void RegisterTransient<TService>() where TService : class;
+
+        /// <summary>
+        /// Registers dependency in a transient lifetime style
+        /// </summary>
+        /// <param name="serviceType">Type of dependency declaration</param>
+        /// <param name="implementationType">Type of dependency implementation</param>
         void RegisterTransient(Type serviceType, Type implementationType);
+
+        /// <summary>
+        /// Registers dependency as a singleton
+        /// </summary>
+        /// <typeparam name="TService">Type of dependency declaration</typeparam>
+        /// <typeparam name="TImplementation">Type of dependency implementation</typeparam>
         void RegisterSingleton<TService, TImplementation>() where TImplementation : class, TService;
+
+        /// <summary>
+        /// Registers an instance of dependency
+        /// </summary>
+        /// <typeparam name="TService">Type of dependency</typeparam>
+        /// <param name="instance">Instance of dependency</param>
         void RegisterInstance<TService>(TService instance) where TService : class;
     }
 
