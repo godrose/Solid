303ab5b59ccb01c461eadb926236a7fee1b4e0fc
godrose <godrose@gmail.com>
2015-10-01 05:59:10 +0300

    Added Memento design pattern

---
+++ Solid.Patterns.Memento/IMemento.cs
@@ -0,0 +1,16 @@
+﻿namespace Solid.Patterns.Memento
+{
+    /// <summary>
+    /// Represents memento object, which is used for restoration of object state
+    /// </summary>
+    /// <typeparam name="T">Type of restoration target</typeparam>
+    public interface IMemento<T>
+    {
+        /// <summary>
+        /// Restores the object to its previous state
+        /// </summary>
+        /// <param name="target">Restoration target</param>
+        /// <returns>Memento that allows to execute action that's inverse to the restoration</returns>
+        IMemento<T> Restore(T target);
+    }
+}
