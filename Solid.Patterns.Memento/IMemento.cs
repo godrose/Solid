a25f771e6f10af1257a02c9f7f06ae985b01bf78
godrose <godrose@gmail.com>
2016-01-03 17:59:00 +0200

    Made Solid.Patterns.Memento portable.

--- Solid.Patterns.Memento/IMemento.cs
+++ Solid.Patterns.Memento/IMemento.cs
@@ -1,13 +1,13 @@
 ﻿namespace Solid.Patterns.Memento
 {
     /// <summary>
-    /// Represents memento object, which is used for restoration of object state
+    /// Represents memento object, which is used for restoration of object state.
     /// </summary>
-    /// <typeparam name="T">Type of restoration target</typeparam>
+    /// <typeparam name="T">Type of restoration target.</typeparam>
     public interface IMemento<T>
     {
         /// <summary>
-        /// Restores the object to its previous state
+        /// Restores the object to its previous state.
         /// </summary>
         /// <param name="target">Restoration target</param>
         /// <returns>Memento that allows to execute action that's inverse to the restoration</returns>
