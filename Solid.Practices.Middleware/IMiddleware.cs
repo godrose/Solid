f91eb476aafc58cbd6e507ae6c2164f29543f806
godrose <godrose@gmail.com>
2016-01-03 12:42:35 +0200

    Added comments.

--- Solid.Practices.Middleware/IMiddleware.cs
+++ Solid.Practices.Middleware/IMiddleware.cs
@@ -2,8 +2,17 @@
 
 namespace Solid.Practices.Middleware
 {
+    /// <summary>
+    /// Represents an interface for middleware that is applied to an IoC container.
+    /// </summary>
     public interface IMiddleware
     {
+        /// <summary>
+        /// Applies the middleware on the specified IoC container.
+        /// </summary>
+        /// <typeparam name="TIocContainer">The type of the IoC container.</typeparam>
+        /// <param name="iocContainer">The IoC container.</param>
+        /// <returns></returns>
         TIocContainer Apply<TIocContainer>(TIocContainer iocContainer) where TIocContainer : class, IIocContainer;
     }
 }
