25e7922d097fe32d15f751e314edd27d3ca9cb75
godrose <godrose@gmail.com>
2016-05-03 09:42:40 +0300

    Removed unneeded packages.

--- Solid.Practices.Composition.Platform.UWP.Tests/CompositionContainerTests.cs
+++ Solid.Practices.Composition.Platform.UWP.Tests/CompositionContainerTests.cs
@@ -1,5 +1,4 @@
 ﻿using System.Linq;
-using LogoFX.Client.Bootstrapping.Adapters.SimpleContainer;
 using NUnit.Framework;
 using Solid.Practices.Composition.Contracts;
 using Solid.Practices.Modularity;
@@ -97,8 +96,7 @@ namespace Solid.Practices.Composition.Platform.UWP.Tests
 
         private static ICompositionModuleCreationStrategy CreateModuleCreationStrategy()
         {
-            //return new ActivatorCreationStrategy();
-            return new ContainerResolutionStrategy(new ExtendedSimpleContainerAdapter());
+            return new ActivatorCreationStrategy();            
         }
     }
 }
--- Solid.Practices.Composition.WINRT.Tests/CompositionContainerTests.cs
+++ Solid.Practices.Composition.WINRT.Tests/CompositionContainerTests.cs
@@ -1,5 +1,4 @@
 ﻿using System.Linq;
-using LogoFX.Client.Bootstrapping.Adapters.SimpleContainer;
 using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
 using Solid.Practices.Composition.Contracts;
 using Solid.Practices.Modularity;
@@ -117,8 +116,7 @@ namespace Solid.Practices.Composition.WINRT.Tests
 
         private static ICompositionModuleCreationStrategy CreateModuleCreationStrategy()
         {
-            //return new ActivatorCreationStrategy();
-            return new ContainerResolutionStrategy(new ExtendedSimpleContainerAdapter());
+            return new ActivatorCreationStrategy();            
         }
     }
 
