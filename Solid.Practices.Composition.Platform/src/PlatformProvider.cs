be37cc1be4ba53438e43d1df59b5e44c6ed9142b
godrose <godrose@gmail.com>
2017-05-13 22:42:07 +0300

    Removed WinRT obsolete code

--- Solid.Practices.Composition.Platform/src/PlatformProvider.cs
+++ Solid.Practices.Composition.Platform/src/PlatformProvider.cs
@@ -1,15 +1,4 @@
-﻿#if NET45 || WINDOWS_UWP
-using System.IO;
-#endif
-#if WINRT
-using Windows.ApplicationModel;
-using Windows.Foundation;
-using Windows.Storage;
-using System;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Text.RegularExpressions;
-#endif
+﻿using System.IO;
 using Solid.Practices.Composition.Contracts;
 
 namespace Solid.Practices.Composition
@@ -24,9 +13,6 @@ namespace Solid.Practices.Composition
 #if WINDOWS_UWP
         UniversalPlatformProvider
 #endif
-#if WINRT
-        WinRTPlatformProvider
-#endif
         : IPlatformProvider
     {
         /// <summary>
@@ -36,12 +22,7 @@ namespace Solid.Practices.Composition
         /// <returns></returns>
         public string[] GetFiles(string path)
         {
-#if WINRT
-            var taskResult = GetFilesInternal(path);
-            return taskResult.Result;
-#else
             return Directory.GetFiles(path);
-#endif
         }
 
         /// <summary>
@@ -52,12 +33,7 @@ namespace Solid.Practices.Composition
         /// <returns></returns>
         public string[] GetFiles(string path, string searchPattern)
         {
-#if WINRT
-            var taskResult = GetFilesInternal(path);
-            return taskResult.Result.Where(t => IsMatch(t, searchPattern)).ToArray();
-#else      
             return System.IO.Directory.GetFiles(path, searchPattern);
-#endif
         }
 
         /// <summary>
@@ -66,11 +42,7 @@ namespace Solid.Practices.Composition
         /// <returns></returns>
         public string GetRootPath()
         {
-#if WINRT
-            return Package.Current.InstalledLocation.Path;            
-#else
             return Directory.GetCurrentDirectory();
-#endif
         }
 
         /// <summary>
@@ -78,25 +50,15 @@ namespace Solid.Practices.Composition
         /// </summary>
         /// <param name="path">The path.</param>
         /// <param name="contents">The text.</param>        
-        public
-#if WINRT
-            async
-#endif
-            void WriteText(string path, string contents)
+        public void WriteText(string path, string contents)
         {
-#if WINRT
-            //supports only current folder
-            var folder = Package.Current.InstalledLocation;
-            var file = await folder.CreateFileAsync(path);
-            await FileIO.WriteTextAsync(file, contents);
-#else
             var fileStream = new FileStream(path, FileMode.Create);
-            using (var textWriter = new StreamWriter(fileStream))             
+            using (var textWriter = new StreamWriter(fileStream))
             {
                 textWriter.Write(contents);
                 textWriter.Flush();
-            }           
-#endif
+            }
+
         }
 
         /// <summary>
@@ -106,113 +68,12 @@ namespace Solid.Practices.Composition
         /// <returns></returns>        
         public string ReadText(string path)
         {
-#if WINRT
-            //support only current folder
-            var folder = Package.Current.InstalledLocation;
-            var fileOperation = folder.GetFileAsync(path);
-            while (fileOperation.Status != AsyncStatus.Completed && fileOperation.Status != AsyncStatus.Canceled &&
-                   fileOperation.Status != AsyncStatus.Error)
-            {
-                Task.Delay(50);
-            }
-            var file = fileOperation.GetResults();
-            var contentsOperation = FileIO.ReadTextAsync(file);
-            while (contentsOperation.Status != AsyncStatus.Completed && contentsOperation.Status != AsyncStatus.Canceled &&
-                   contentsOperation.Status != AsyncStatus.Error)
-            {
-                Task.Delay(50);
-            }
-            return contentsOperation.GetResults();
-#else
             var fileStream = new FileStream(path, FileMode.Open);
             using (var textReader = new StreamReader(fileStream))
             {
                 var contents = textReader.ReadToEnd();
                 return contents;
-            }                          
-#endif
-        }
-
-#if WINRT
-
-        private const int Delay = 50;
-
-        private static async Task<string[]> GetFilesInternal(string path)
-        {
-            path = path.TrimEnd('.');
-            var folderOperation = StorageFolder.GetFolderFromPathAsync(path);
-            while (folderOperation.Status == AsyncStatus.Started)
-            {
-                await Task.Delay(Delay);                
-            }
-            if (folderOperation.Status == AsyncStatus.Error)
-            {
-                throw folderOperation.ErrorCode;
-            }
-            var folder = folderOperation.GetResults();
-            var filesOperation = folder.GetFilesAsync();
-            while (filesOperation.Status == AsyncStatus.Started)
-            {
-                await Task.Delay(Delay);
-            }
-            if (filesOperation.Status == AsyncStatus.Error)
-            {
-                throw filesOperation.ErrorCode;
-            }
-            var files = filesOperation.GetResults();
-            return files.Select(t => t.Name).ToArray();
-        }
-
-        private bool IsMatch(string name, string pattern)
-        {            
-            Regex regex = FindFilesPatternToRegex.Convert(pattern);
-            var isMatch = regex.IsMatch(name);
-            return isMatch;
-        }
-
-        private static class FindFilesPatternToRegex
-        {
-            private static Regex HasQuestionMarkRegEx = new Regex(@"\?", RegexOptions.None);
-            private static Regex IllegalCharactersRegex = new Regex("[" + @"\/:<>|" + "\"]", RegexOptions.None);
-            private static Regex CatchExtensionRegex = new Regex(@"^\s*.+\.([^\.]+)\s*$", RegexOptions.None);
-            private static string NonDotCharacters = @"[^.]*";
-            public static Regex Convert(string pattern)
-            {
-                if (pattern == null)
-                {
-                    throw new ArgumentNullException();
-                }
-                pattern = pattern.Trim();
-                if (pattern.Length == 0)
-                {
-                    throw new ArgumentException("Pattern is empty.");
-                }
-                if (IllegalCharactersRegex.IsMatch(pattern))
-                {
-                    throw new ArgumentException("Pattern contains illegal characters.");
-                }
-                bool hasExtension = CatchExtensionRegex.IsMatch(pattern);
-                bool matchExact = false;
-                if (HasQuestionMarkRegEx.IsMatch(pattern))
-                {
-                    matchExact = true;
-                }
-                else if (hasExtension)
-                {
-                    matchExact = CatchExtensionRegex.Match(pattern).Groups[1].Length != 3;
-                }
-                string regexString = Regex.Escape(pattern);
-                regexString = "^" + Regex.Replace(regexString, @"\\\*", ".*");
-                regexString = Regex.Replace(regexString, @"\\\?", ".");
-                if (!matchExact && hasExtension)
-                {
-                    regexString += NonDotCharacters;
-                }
-                regexString += "$";
-                Regex regex = new Regex(regexString, RegexOptions.None | RegexOptions.IgnoreCase);
-                return regex;
             }
         }
-#endif
     }
 }
