2131184069d96e9ae7c0100d056d8a83fde3fc22
godrose <godrose@gmail.com>
2017-05-20 00:04:58 +0300

    Finalized adding netstandard support

---
+++ Solid.Patterns.ChainOfResponsibility/src/IChainElement.cs
@@ -0,0 +1,32 @@
+﻿namespace Solid.Patterns.ChainOfResponsibility
+{
+    /// <summary>
+    /// Represents chain element which can be used in the chain-of-responsibility 
+    /// design pattern implementation.
+    /// </summary>
+    /// <typeparam name="TData">The type of the data.</typeparam>    
+    public interface IChainElement<TData> : ICanSetSuccessor<IChainElement<TData>>
+    {
+        /// <summary>
+        /// Handles the specified data.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        void Handle(TData data);        
+    }
+
+    /// <summary>
+    /// Represents chain element which can be used in the chain-of-responsibility 
+    /// design pattern implementation.
+    /// </summary>
+    /// <typeparam name="TData">The type of the data.</typeparam>
+    /// <typeparam name="TResult">The type of the return value.</typeparam>    
+    public interface IChainElement<TData, TResult> : ICanSetSuccessor<IChainElement<TData, TResult>>
+    {
+        /// <summary>
+        /// Handles the specified data.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        /// <returns></returns>
+        TResult Handle(TData data);        
+    }
+}
