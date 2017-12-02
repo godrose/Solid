b534e12c481720a73d56896df26913db4a01017b
Gennady Verdel <godrose@gmail.com>
2017-12-02 08:14:02 +0200

    Added .netstandard projects for Composition.Web

---
+++ Solid.Practices.Composition.Web/src/AssembliesResolver.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Solid.Practices.Composition.Contracts;
+
+namespace Solid.Practices.Composition.Web
+{
+    /// <summary>
+    /// Assemblies resolver.
+    /// </summary>
+    public class AssembliesResolver : AssembliesResolverBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AssembliesResolver"/> class.
+        /// </summary>        
+        /// <param name="assemblySourceProvider">The assembly source provider.</param>
+        public AssembliesResolver(IAssemblySourceProvider assemblySourceProvider) : base(assemblySourceProvider)
+        {
+
+        }
+
+        /// <summary>
+        /// Override this method to retrieve platform-specific root assemblies.
+        /// </summary>
+        /// <returns>Collection of assemblies.</returns>
+        protected override IEnumerable<Assembly> GetRootAssemblies()
+        {
+            return new Assembly[] { };
+        }
+    }
+}
---
+++ Solid.Practices.Composition.Web/src/ServerAssemblySourceProvider.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+
+namespace Solid.Practices.Composition.Web
+{
+    /// <summary>
+    /// Retrieves list of the assemblies to be 
+    /// inspected for application elements in the server side.
+    /// </summary>
+    public class ServerAssemblySourceProvider : AssemblySourceProviderBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ServerAssemblySourceProvider"/> class.
+        /// </summary>
+        /// <param name="rootPath">The root path.</param>
+        public ServerAssemblySourceProvider(string rootPath) : base(rootPath)
+        {
+        }
+
+        /// <summary>
+        /// Returns the list of namespaces to be looked for during assembly discovery.
+        /// </summary>
+        /// <returns></returns>
+        protected override string[] ResolveNamespaces()
+        {
+            return AssemblyLoadingManager.ServerNamespaces().ToArray();
+        }
+    }
+}
