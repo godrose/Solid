8bbe31b52be1cb2d5ba4b8a10f4972d69646d723
Gennady Verdel <godrose@gmail.com>
2017-12-02 07:50:27 +0200

    Adjusted assemblies resolver implementation for server #26

--- Solid.Practices.Composition.Web/AssembliesResolver.cs
+++ Solid.Practices.Composition.Web/AssembliesResolver.cs
@@ -1,58 +1,30 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Web;
-using System.Web.Http.Dispatcher;
 using Solid.Practices.Composition.Contracts;
 
 namespace Solid.Practices.Composition.Web
 {
     /// <summary>
-    /// Assemblies resolver for server part of web applications.
+    /// Assemblies resolver.
     /// </summary>
-    public class AssembliesResolver : AssembliesResolverBase, IAssembliesResolver
+    public class AssembliesResolver : AssembliesResolverBase
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="AssembliesResolver"/> class.
         /// </summary>        
         /// <param name="assemblySourceProvider">The assembly source provider.</param>
-        public AssembliesResolver(
-            IAssemblySourceProvider assemblySourceProvider) : base(assemblySourceProvider)
-        {           
+        public AssembliesResolver(IAssemblySourceProvider assemblySourceProvider) : base(assemblySourceProvider)
+        {
+
         }
 
         /// <summary>
         /// Override this method to retrieve platform-specific root assemblies.
         /// </summary>
-        /// <returns>Collection of assemblies</returns>
+        /// <returns>Collection of assemblies.</returns>
         protected override IEnumerable<Assembly> GetRootAssemblies()
         {
-            return new []
-            {
-                GetEntryAssembly()
-            };
+            return new Assembly[] { };
         }
-
-        ICollection<Assembly> IAssembliesResolver.GetAssemblies()
-        {
-            return ((IAssembliesReadOnlyResolver) this).GetAssemblies().ToList();
-        }
-
-        private static Assembly GetEntryAssembly()
-        {
-            if (HttpContext.Current == null ||
-                HttpContext.Current.ApplicationInstance == null)
-            {
-                return null;
-            }
-
-            var type = HttpContext.Current.ApplicationInstance.GetType();
-            while (type != null && type.BaseType != null && type.BaseType.Name != "HttpApplication")
-            {
-                type = type.BaseType;
-            }
-
-            return type == null ? null : type.Assembly;
-        }       
     }
 }
