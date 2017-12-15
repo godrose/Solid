e5e35ef47405958451882a5c72e0dee3428c5378
Gennady Verdel <godrose@gmail.com>
2017-12-15 10:45:46 +0200

    Moved composition.client files to the correspondent location #27

---
+++ Solid.Practices.Composition.Client/src/AssembliesResolver.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Solid.Practices.Composition.Contracts;
+
+namespace Solid.Practices.Composition.Client
+{
+    /// <summary>
+    /// Assemblies resolver for client part of desktop applications.
+    /// </summary>
+    public class AssembliesResolver : AssembliesResolverBase
+    {
+        private readonly Type _entryType;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AssembliesResolver"/> class.
+        /// </summary>
+        /// <param name="entryType">Type of the entry.</param>
+        /// <param name="assemblySourceProvider">The assembly source provider.</param>
+        public AssembliesResolver(Type entryType,
+            IAssemblySourceProvider assemblySourceProvider) : base(assemblySourceProvider)
+        {
+            _entryType = entryType;
+        }
+
+        /// <summary>
+        /// Override this method to retrieve platform-specific root assemblies.
+        /// </summary>
+        /// <returns>Collection of assemblies.</returns>
+        protected override IEnumerable<Assembly> GetRootAssemblies()
+        {
+            return Enumerable.Repeat(
+                _entryType.GetTypeInfo()
+                    .Assembly, 1);
+        }
+    }
+}
