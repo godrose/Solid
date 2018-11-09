3129327e758d106e0c35f5347757e64c7970f24f
Gennady Verdel <godrose@gmail.com>
2018-11-09 10:19:59 +0200

    #33 Dropped .NETStandard20 from Tests

--- Solid.Practices.Composition.Container.Tests/Properties/AssemblyInfo.cs
+++ Solid.Practices.Composition.Container.Tests/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Solid.Practices.Composition.Container.NETStandard20.Tests")]
+[assembly: AssemblyTitle("Solid.Practices.Composition.Container.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Solid.Practices.Composition.Container.NETStandard20.Tests")]
+[assembly: AssemblyProduct("Solid.Practices.Composition.Container.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- Solid.Practices.Composition.Tests/Properties/AssemblyInfo.cs
+++ Solid.Practices.Composition.Tests/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Solid.Practices.Composition.NETStandard20.Tests")]
+[assembly: AssemblyTitle("Solid.Practices.Composition.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Solid.Practices.Composition.NETStandard20.Tests")]
+[assembly: AssemblyProduct("Solid.Practices.Composition.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
