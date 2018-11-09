ac6cdce6b4fecb9e3777af2e5b72c4857ec75112
Gennady Verdel <godrose@gmail.com>
2018-11-09 10:04:38 +0200

    #33 - Cleaned up Tests

--- Solid.Practices.Composition.Tests/CompositionContainerTests.cs
+++ Solid.Practices.Composition.Tests/CompositionContainerTests.cs
@@ -1,80 +1,120 @@
-﻿using System.Linq;
-using NUnit.Framework;
-using PCLStorage;
+﻿using System.IO;
+using System.Linq;
+using FluentAssertions;
+using Solid.Practices.Composition.Contracts;
+using Solid.Practices.Modularity;
+using Xunit;
 
 namespace Solid.Practices.Composition.Tests
-{
-    [TestFixture]
-    class CompositionContainerTests
+{    
+    public class CompositionContainerTests
     {
-        [Test]
+        static CompositionContainerTests()
+        {
+            PlatformProvider.Current = new NetStandardPlatformProvider();
+        }
+
+        [Fact]
         public void RootPathContainsCompositionModules_CompositionModulesAreImported()
         {
-            var rootPath = GetRootPath();
-
-            var compositionContainer = new CompositionContainer(rootPath);
+            var rootPath = GetCurrentDirectory();
+            
+            ICompositionContainer<ICompositionModule> compositionContainer = CreateCompositionContainer<ICompositionModule>(rootPath);
             compositionContainer.Compose();
 
             var modules = compositionContainer.Modules;
             var modulesCount = modules.Count();
-            Assert.AreEqual(1, modulesCount);
+            modulesCount.Should().Be(3);            
         }
 
-        [Test]
+        [Fact]
         public void RootPathContainsCustomModules_CustomModulesAreImported()
         {
-            var rootPath = GetRootPath();
+            var rootPath = GetCurrentDirectory();
 
-            var compositionContainer = new CompositionContainer<ICustomModule>(rootPath);
+            ICompositionContainer<ICustomModule> compositionContainer = CreateCompositionContainer<ICustomModule>(rootPath);
             compositionContainer.Compose();
 
             var modules = compositionContainer.Modules;
             var modulesCount = modules.Count();
-            Assert.AreEqual(2, modulesCount);
+            modulesCount.Should().Be(2);
         }
 
-        [Test]
+        [Fact]
         public void RootPathContainsCustomModulesAndCorrectPrefixIsUsed_CustomModulesAreImported()
         {
-            var rootPath = GetRootPath();
+            var rootPath = GetCurrentDirectory();
 
-            var compositionContainer = new CompositionContainer<ICustomModule>(rootPath, new[] { "Solid" });
+            ICompositionContainer<ICustomModule> compositionContainer = CreateCompositionContainer<ICustomModule>(rootPath, new[] { "Solid" });
             compositionContainer.Compose();
 
             var modules = compositionContainer.Modules;
             var modulesCount = modules.Count();
-            Assert.AreEqual(2, modulesCount);
+            modulesCount.Should().Be(2);
         }
 
-        [Test]
+        [Fact]
         public void RootPathContainsCustomModulesAndIncorrectPrefixIsUsed_CustomModulesAreImported()
         {
-            var rootPath = GetRootPath();
+            var rootPath = GetCurrentDirectory();
 
-            var compositionContainer = new CompositionContainer<ICustomModule>(rootPath, new[] { "Incorrect" });
+            ICompositionContainer<ICustomModule> compositionContainer = CreateCompositionContainer<ICustomModule>(rootPath, new[] { "Incorrect" });
             compositionContainer.Compose();
 
             var modules = compositionContainer.Modules;
             var modulesCount = modules.Count();
-            Assert.AreEqual(0, modulesCount);
+            modulesCount.Should().Be(0);
         }
 
-        [Test]
+        [Fact]
         public void RootPathContainsCustomModulesAndNoPrefixIsUsed_CustomModulesAreImported()
         {
-            var rootPath = GetRootPath();
+            var rootPath = GetCurrentDirectory();
 
-            var compositionContainer = new CompositionContainer<ICustomModule>(rootPath);
+            ICompositionContainer<ICustomModule> compositionContainer = CreateCompositionContainer<ICustomModule>(rootPath);
             compositionContainer.Compose();
 
             var modules = compositionContainer.Modules;
             var modulesCount = modules.Count();
-            Assert.AreEqual(2, modulesCount);
+            modulesCount.Should().Be(2);
         }
 
-        private string GetRootPath()
-        {            
-            return FileSystem.Current.LocalStorage.Path;            
+        [Fact]
+        public void RootPathContainsOtherModules_OtherModulesAreImported()
+        {
+            var rootPath = GetCurrentDirectory();
+
+            ICompositionContainer<IAnotherModule> compositionContainer = CreateCompositionContainer<IAnotherModule>(rootPath);
+            compositionContainer.Compose();
+
+            var modules = compositionContainer.Modules;
+            var modulesCount = modules.Count();
+            modulesCount.Should().Be(1);
+        }        
+
+        private static string GetCurrentDirectory()
+        {
+            return Directory.GetCurrentDirectory();
+        }
+
+        private static CompositionContainer<TModule> CreateCompositionContainer<TModule>(string rootPath) 
+            where TModule : ICompositionModule
+        {
+            return new CompositionContainer<TModule>(CreateModuleCreationStrategy(),
+                new FileSystemBasedAssemblyLoadingStrategy(rootPath));
+        }
+
+        private static CompositionContainer<TModule> CreateCompositionContainer<TModule>(string rootPath, string[] prefixes)
+            where TModule : ICompositionModule
+        {
+            return new CompositionContainer<TModule>(CreateModuleCreationStrategy(),
+                new FileSystemBasedAssemblyLoadingStrategy(rootPath, prefixes));
+        }
+
+        private static ICompositionModuleCreationStrategy CreateModuleCreationStrategy()
+        {
+            return new ActivatorCreationStrategy();            
         }
     }
+
 }
