d8f90edc592697c75d3979e89c1d5f9c25929412
Gennady Verdel <godrose@gmail.com>
2018-11-16 16:28:25 +0200

    Added missing comments

--- Solid.Practices.Composition/CustomAssemblySourceProvider.cs
+++ Solid.Practices.Composition/CustomAssemblySourceProvider.cs
@@ -2,18 +2,24 @@
 {
     /// <summary>
     /// Allows injecting custom namespaces to retrieve list of the assemblies to be 
-    /// inspected for application elements,
+    /// inspected for application elements.
     /// </summary>
     /// <seealso cref="AssemblySourceProviderBase" />
     public sealed class CustomAssemblySourceProvider : AssemblySourceProviderBase
     {
         private readonly string[] _endings;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CustomAssemblySourceProvider"/> class.
+        /// </summary>
+        /// <param name="rootPath"></param>
+        /// <param name="endings"></param>
         public CustomAssemblySourceProvider(string rootPath, string[] endings) : base(rootPath)
         {
             _endings = endings;
         }
 
+        /// <inheritdoc />
         protected override string[] ResolveNamespaces() => _endings;
     }
 }
