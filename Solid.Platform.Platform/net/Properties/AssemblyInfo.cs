8e8b09519f5237f07dce331153af1cc9310e89e5
Gennady Verdel <godrose@gmail.com>
2018-11-16 19:50:18 +0200

    #31 Fixed assembly names

--- Solid.Platform.Platform/net/Properties/AssemblyInfo.cs
+++ Solid.Platform.Platform/net/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Solid.Platform")]
+[assembly: AssemblyTitle("Solid.Platform.Platform")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Solid.Platform")]
+[assembly: AssemblyProduct("Solid.Platform.Platform")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
