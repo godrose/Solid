5256f363badfa1a00c476df9a4cea2c203c1dfa9
Gennady Verdel <godrose@gmail.com>
2018-11-16 16:07:22 +0200

    #31 Added custom assembly source provider

---
+++ Solid.Practices.Composition/CustomAssemblySourceProvider.cs
@@ -0,0 +1,19 @@
+﻿namespace Solid.Practices.Composition
+{
+    /// <summary>
+    /// Allows injecting custom namespaces to retrieve list of the assemblies to be 
+    /// inspected for application elements,
+    /// </summary>
+    /// <seealso cref="AssemblySourceProviderBase" />
+    public sealed class CustomAssemblySourceProvider : AssemblySourceProviderBase
+    {
+        private readonly string[] _endings;
+
+        public CustomAssemblySourceProvider(string rootPath, string[] endings) : base(rootPath)
+        {
+            _endings = endings;
+        }
+
+        protected override string[] ResolveNamespaces() => _endings;
+    }
+}
