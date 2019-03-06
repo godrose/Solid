48b88e95ce41359b16aa9dc805bd995f4825098a
Gennady Verdel <godrose@gmail.com>
2019-03-06 10:20:51 +0200

    #36 Added support for wildcard matching

--- Solid.Core.Tests/PatternsCalculatorTests.cs
+++ Solid.Core.Tests/PatternsCalculatorTests.cs
@@ -26,50 +26,87 @@ namespace Solid.Core.Tests
             {
                 new object[]
                 {
-                    new InputData(new[] {"Prefix"}, new[] {"Namespace"}, new[] {".ext"}),
-                    new[] {new PatternDescription("Prefix", "Namespace", ".ext")}
+                    new InputData(new[] {"Prefix"}, new[] {"Namespace"}, new[] {"ext"}),
+                    new[] {new PatternDescription("Prefix", "Namespace", "ext")}
                 },
                 new object[]
                 {
-                    new InputData(new[] {"Prefix"}, new[] {"Namespace1", "Namespace2"}, new[] {".ext"}),
+                    new InputData(new[] {"Prefix"}, new[] {"Namespace1", "Namespace2"}, new[] {"ext"}),
                     new[]
                     {
-                        new PatternDescription("Prefix", "Namespace1", ".ext"),
-                        new PatternDescription("Prefix", "Namespace2", ".ext")
+                        new PatternDescription("Prefix", "Namespace1", "ext"),
+                        new PatternDescription("Prefix", "Namespace2", "ext")
                     }
                 },
                 new object[]
                 {
-                    new InputData(new[] {"Prefix"}, new[] {"Namespace1", "Namespace2"}, new[] {".ext1", ".ext2"}),
+                    new InputData(new[] {"Prefix"}, new[] {"Namespace1", "Namespace2"}, new[] {"ext1", "ext2"}),
                     new[]
                     {
-                        new PatternDescription("Prefix", "Namespace1", ".ext1"),
-                        new PatternDescription("Prefix", "Namespace1", ".ext2"),
-                        new PatternDescription("Prefix", "Namespace2", ".ext1"),
-                        new PatternDescription("Prefix", "Namespace2", ".ext2")
+                        new PatternDescription("Prefix", "Namespace1", "ext1"),
+                        new PatternDescription("Prefix", "Namespace1", "ext2"),
+                        new PatternDescription("Prefix", "Namespace2", "ext1"),
+                        new PatternDescription("Prefix", "Namespace2", "ext2")
                     }
                 },
                 new object[]
                 {
-                    new InputData(new[] {"Prefix1", "Prefix2"}, new[] {"Namespace1", "Namespace2"}, new[] {".ext1", ".ext2"}),
+                    new InputData(new[] {"Prefix1", "Prefix2"}, new[] {"Namespace1", "Namespace2"}, new[] {"ext1", "ext2"}),
                     new[]
                     {
-                        new PatternDescription("Prefix1", "Namespace1", ".ext1"),
-                        new PatternDescription("Prefix1", "Namespace1", ".ext2"),
-                        new PatternDescription("Prefix1", "Namespace2", ".ext1"),
-                        new PatternDescription("Prefix1", "Namespace2", ".ext2"),
-                        new PatternDescription("Prefix2", "Namespace1", ".ext1"),
-                        new PatternDescription("Prefix2", "Namespace1", ".ext2"),
-                        new PatternDescription("Prefix2", "Namespace2", ".ext1"),
-                        new PatternDescription("Prefix2", "Namespace2", ".ext2")
+                        new PatternDescription("Prefix1", "Namespace1", "ext1"),
+                        new PatternDescription("Prefix1", "Namespace1", "ext2"),
+                        new PatternDescription("Prefix1", "Namespace2", "ext1"),
+                        new PatternDescription("Prefix1", "Namespace2", "ext2"),
+                        new PatternDescription("Prefix2", "Namespace1", "ext1"),
+                        new PatternDescription("Prefix2", "Namespace1", "ext2"),
+                        new PatternDescription("Prefix2", "Namespace2", "ext1"),
+                        new PatternDescription("Prefix2", "Namespace2", "ext2")
                     }
+                }
+            };
+
+        public static IEnumerable<object[]> EmptyCollectionsData =>
+            new List<object[]>
+            {
+                new object[]
+                {
+                    new InputData(new string[] {}, new[] {"Namespace"}, new[] {"ext"}),
+                    new[] {new PatternDescription("*", "Namespace", "ext")}
+                },
+                new object[]
+                {
+                    new InputData(null, new[] {"Namespace"}, new[] {"ext"}),
+                    new[] {new PatternDescription("*", "Namespace", "ext")}
+                },
+                new object[]
+                {
+                    new InputData(new string[] {}, new string[] {}, new[] {"ext"}),
+                    new[] {new PatternDescription("*", "*", "ext")}
+                },
+                new object[]
+                {
+                    new InputData(null, null, new[] {"ext"}),
+                    new[] {new PatternDescription("*", "*", "ext")}
+                },
+                new object[]
+                {
+                    new InputData(new string[] {}, new string[] {}, new string[] {}),
+                    new[] {new PatternDescription("*", "*", "*")}
+                },
+                new object[]
+                {
+                    new InputData(null, null, null),
+                    new[] {new PatternDescription("*", "*", "*")}
                 }
             };
 
         [Theory]
         [MemberData(nameof(NonEmptyCollectionsData))]
-        public void Search_AllCollectionsAreNonEmpty_ExplicitPathsAreReturned(InputData input, PatternDescription[] expectedOutput)
-        {           
+        [MemberData(nameof(EmptyCollectionsData))]
+        public void Calculate_VariousInputsAreSupplied_ExpectedOutputsAreReturned(InputData input,
+            PatternDescription[] expectedOutput)
+        {
             var searcher = new PatternsCalculator();
             var paths = searcher.Calculate(input.Prefixes, input.Namespaces, input.Extensions).ToArray();
 
@@ -86,15 +123,19 @@ namespace Solid.Core.Tests
             Postfix = postfix;
         }
 
-        public string Prefix { get; private set; }
-        public string Contents { get; private set; }
-        public string Postfix { get; private set; }
+        public string Prefix { get; }
+        public string Contents { get; }
+        public string Postfix { get; }
     }
 
     internal class PatternsCalculator
-    {
+    {        
         public IEnumerable<PatternDescription> Calculate(string[] prefixes, string[] namespaces, string[] extensions)
         {
+            prefixes = prefixes.Patch();
+            namespaces = namespaces.Patch();
+            extensions = extensions.Patch();
+
             foreach (var prefix in prefixes)
             {
                 foreach (var ns in namespaces)
@@ -105,6 +146,16 @@ namespace Solid.Core.Tests
                     }
                 }
             }
+        }        
+    }
+
+    internal static class CollectionExtensions
+    {
+        private static string WildCard = "*";
+
+        internal static string[] Patch(this string[] input)
+        {
+            return input == null || input.Length == 0 ? new[] {WildCard} : input;
         }
     }
 }
