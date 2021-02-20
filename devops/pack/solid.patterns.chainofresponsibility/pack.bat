27d85c545d69134a296c604c185f1ce3dd6945be
Gennady Verdel <godrose@gmail.com>
2021-02-20 20:45:16 +0200

    feat(devops): adding automated nuget creation for solid.chainofresponsibility #8

---
+++ devops/pack/solid.patterns.chainofresponsibility/pack.bat
@@ -0,0 +1,8 @@
+cd contents
+rmdir /Q /S lib
+mkdir lib
+cd lib
+mkdir netstandard2.0\
+robocopy ../../../../../Bin/netstandard/Release netstandard2.0 Solid.Patterns.ChainOfResponsibility.* /E
+cd ../../
+nuget pack contents/Solid.Patterns.ChainOfResponsibility.nuspec -OutputDirectory ../../../output
