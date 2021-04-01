2b6113bae560f9d17e7921384e1b7d4ad969f92d
Gennady Verdel <godrose@gmail.com>
2021-04-01 21:02:29 +0300

    devops: storing packages as artifacts #8

--- devops/publish/copy-all.bat
+++ devops/publish/copy-all.bat
@@ -1,6 +1,1 @@
-SET package_version=%1
-SET target=../../../packages/Tests-All
-for /d %%d in (../pack/*.*) do (
-	echo %%d 
-	call copy-single.bat %%d %package_version% %target%	
-)
+call copy-all-params ../../../packages/Tests-All
