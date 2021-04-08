1956a344c33f3b26693cc8c4c57891ef4d3c798c
Gennady Verdel <godrose@gmail.com>
2021-04-08 18:28:52 +0300

    Devops/update version closes #64 (#65)
    * devops: updating version in nuget manifest files #64
    
    * devops: updating version in ci file #64
    
    * chore: minor
    
    * devops: patching version inside assembly info #64
    
    * devops: patching version for sdk-style projects #64
    
    * devops: refactored update tool #64

---
+++ devops/update/UpdateUtil/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace UpdateUtil
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var prefix = args[0];
+            var version = args.Length >= 2 ? args[1] : string.Empty;
+            if (version == string.Empty)
+            {
+                Console.WriteLine("No version provided");
+                return;
+            }
+            var versionInfo = new VersionInfo(version);
+            var handlers = new FileTypeHandlerBase[]
+            {
+                new SdkProjectFileTypeHandler(), 
+                new AssemblyInfoFileTypeHandler(), 
+                new CIFileTypeHandler(),
+                new ManifestFileTypeHandler()
+            };
+            foreach (var handler in handlers)
+            {
+                handler.UpdateFiles(prefix, versionInfo);
+            }
+        }
+    }
+}
