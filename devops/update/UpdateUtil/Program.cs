1c9ef1ce96c6c6ed7ddccff553376bef03a59444
Gennady Verdel <godrose@gmail.com>
2021-04-08 17:41:33 +0300

    devops: patching version for sdk-style projects #64

--- devops/update/UpdateUtil/Program.cs
+++ devops/update/UpdateUtil/Program.cs
@@ -20,10 +20,10 @@ namespace UpdateUtil
                 return;
             }
             var versionInfo = new VersionInfo(version);
+            UpdateProjectFiles(prefix, versionInfo);
             UpdateAssemblyInfoFiles(prefix, versionInfo);
             UpdateCIFile(versionInfo);
             UpdateManifestFiles(prefix, versionInfo);
-            
         }
 
         private static void UpdateManifestFiles(string prefix, VersionInfo versionInfo)
@@ -100,6 +100,46 @@ namespace UpdateUtil
                     UseShellExecute = false
                 };
                 Process.Start(startInfo).WaitForExit();
+            }
+        }
+
+        private static void UpdateProjectFiles(string prefix, VersionInfo versionInfo)
+        {
+            GoUp(4);
+            var version = versionInfo.VersionCore;
+            var projectFiles =
+                Directory.GetFiles(Directory.GetCurrentDirectory(), "*.csproj", SearchOption.AllDirectories)
+                    .Where(t => t.Contains(prefix));
+            Cd("devops");
+            NavigateToBin();
+
+            foreach (var projectFile in projectFiles)
+            {
+                if (projectFile.Contains("templatepack"))
+                {
+                    continue;
+                }
+                var doc = new XmlDocument();
+                doc.Load(projectFile);
+                var firstPropertyGroup = doc.GetElementsByTagName("Project")[0]["PropertyGroup"];
+                var targetFrameworkElement = firstPropertyGroup?.GetElementsByTagName("TargetFramework")[0];
+                if (targetFrameworkElement == null)
+                {
+                    continue;
+                }
+
+                if (!targetFrameworkElement.InnerText.StartsWith("netcoreapp") &&
+                    !targetFrameworkElement.InnerText.StartsWith("net5") &&
+                    !targetFrameworkElement.InnerText.StartsWith("netstandard"))
+                {
+                    continue;
+                }
+                var versionElement = doc.GetElementsByTagName("Project")[0]["PropertyGroup"].GetElementsByTagName("Version")[0];
+                if (versionElement != null)
+                {
+                    versionElement.InnerText = version;
+                }
+                doc.Save(projectFile);
             }
         }
 
