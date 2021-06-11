618fda2b671ea7c3799a33182c4c052a56225290
Gennady Verdel <godrose@gmail.com>
2021-06-11 15:32:48 +0300

    feat(use-middleware-by-type): added happy path scenario #68

---
+++ Solid.Extensibility.Specs/Middleware Types Wrapper.feature
@@ -0,0 +1,10 @@
+﻿Feature: Middleware Types Wrapper
+	As an app developer
+	I want to be able to use middlewares by specifying their types only
+	In order to support more advanced scenarios during bootstrapping
+
+Scenario: Using creatable middleware by type should raise no issues during middleware creation
+	When The middleware types wrapper is created
+	And I use a creatable middleware by specifying its type only
+	And I ensure the middlewares are created
+	Then This middleware is created successfully
---
+++ Solid.Extensibility.Specs/MiddlewareTypesSteps.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Attest.Testing.SpecFlow;
+using BoDi;
+using FluentAssertions;
+using JetBrains.Annotations;
+using Solid.IoC.Adapters.BoDi;
+using Solid.Patterns.Builder;
+using Solid.Practices.IoC;
+using Solid.Practices.Middleware;
+using TechTalk.SpecFlow;
+
+namespace Solid.Extensibility.Specs
+{
+    [Binding]
+    [UsedImplicitly]
+    internal sealed class MiddlewareTypesSteps
+    {
+        private MiddlewaresReadOnlyCollection<ExtensibleByTypeObject> _middlewaresReadOnlyCollection;
+
+        //TODO: Move to the data store
+        private MiddlewareTypesWrapper<ExtensibleByTypeObject> _middlewareTypesWrapper;
+        private readonly ObjectContainerAdapter _containerAdapter;
+
+        public MiddlewareTypesSteps(ObjectContainer objectContainer)
+        {
+            _containerAdapter = new ObjectContainerAdapter(objectContainer);
+        }
+
+        [When(@"The middleware types wrapper is created")]
+        [UsedImplicitly]
+        public void WhenTheMiddlewareTypesWrapperIsCreated()
+        {
+            _middlewareTypesWrapper = new MiddlewareTypesWrapper<ExtensibleByTypeObject>(_containerAdapter);
+        }
+
+        [When(@"I use a creatable middleware by specifying its type only")]
+        public void WhenIUseACreatableMiddlewareBySpecifyingItsTypeOnly()
+        {
+            _middlewareTypesWrapper.Use<CreatableMiddleware>();
+        }
+
+        [When(@"I ensure the middlewares are created")]
+        public void WhenIEnsureTheMiddlewaresAreCreated()
+        {
+            _middlewaresReadOnlyCollection = _middlewareTypesWrapper.Build();
+        }
+
+        [Then(@"This middleware is created successfully")]
+        public void ThenThisMiddlewareIsCreatedSuccessfully()
+        {
+            _middlewaresReadOnlyCollection
+                .Middlewares
+                .Should()
+                .ContainSingle(t => t is CreatableMiddleware);
+        }
+    }
+
+    internal class MiddlewareTypesWrapper<TExtensible> :
+        IBuilder<MiddlewaresReadOnlyCollection<TExtensible>>
+        where TExtensible : class
+    {
+        private readonly IIocContainer _iocContainer;
+        private readonly List<Type> _middlewareTypes = new List<Type>();
+
+        public MiddlewareTypesWrapper(
+            IIocContainer iocContainer)
+        {
+            _iocContainer = iocContainer;
+        }
+
+        public MiddlewaresReadOnlyCollection<TExtensible> Build()
+        {
+            return new MiddlewaresReadOnlyCollection<TExtensible>(
+                _middlewareTypes.Select(t => (IMiddleware<TExtensible>) _iocContainer.Resolve(t)));
+        }
+
+        public void Use<TExtension>() where TExtension : class, IMiddleware<TExtensible>
+        {
+            _middlewareTypes.Add(typeof(TExtension));
+            _iocContainer.RegisterSingleton<TExtension>();
+        }
+    }
+
+    internal class MiddlewaresReadOnlyCollection<TExtensible> where TExtensible : class
+    {
+        public MiddlewaresReadOnlyCollection(IEnumerable<IMiddleware<TExtensible>> middlewares)
+        {
+            Middlewares = middlewares.ToArray();
+        }
+
+        public IEnumerable<IMiddleware<TExtensible>> Middlewares { get; }
+    }
+
+    internal class MiddlewareTypeScenarioDataStore : ScenarioDataStoreBase
+    {
+        public MiddlewareTypeScenarioDataStore(ScenarioContext scenarioContext) :
+            base(scenarioContext)
+        {
+        }
+    }
+
+    internal class ExtensibleByTypeObject
+    {
+    }
+
+    internal class CreatableMiddleware : IMiddleware<ExtensibleByTypeObject>
+    {
+        public ExtensibleByTypeObject Apply(ExtensibleByTypeObject @object)
+        {
+            return @object;
+        }
+    }
+}
