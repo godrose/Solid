b6de63344e8fe21a76cdd1aded4b584565cb051f
Gennady Verdel <godrose@gmail.com>
2021-06-14 19:15:32 +0300

    chore: bump version

--- Solid.Common.Platform/uwp/Properties/AssemblyInfo.cs
+++ Solid.Common.Platform/uwp/Properties/AssemblyInfo.cs
@@ -20,6 +20,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.3.0")]
-[assembly: AssemblyFileVersion("2.3.0")]
+[assembly: AssemblyVersion("2.3.1")]
+[assembly: AssemblyFileVersion("2.3.1")]
 [assembly: ComVisible(false)]
