8dcadc548b82b1b417109e949df7c700b0879a1e
Gennady Verdel <godrose@gmail.com>
2021-09-24 10:17:42 +0300

    chore: fixed living doc location

--- devops/test/test-specs-single.bat
+++ devops/test/test-specs-single.bat
@@ -4,4 +4,5 @@ dotnet test %1.csproj -c Release
 rem TODO: Provide more generic and reusable way
 cd ./Bin/Release
 livingdoc test-assembly %1.dll -t TestExecution.json --work-item-url-template https://github.com/godrose/Solid/issues/{id} --work-item-prefix WI
+copy /Y LivingDoc.html "../../../../output/%1.LivingDoc.html"
 cd ../../../../devops/test
