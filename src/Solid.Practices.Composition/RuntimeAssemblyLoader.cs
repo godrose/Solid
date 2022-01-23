7065ca290f6167f60b9dd795292463eef7ebeb0b
Gennady Verdel <godrose@gmail.com>
2022-01-23 05:38:13 +0200

    chore: added missing documentation

--- src/Solid.Practices.Composition/RuntimeAssemblyLoader.cs
+++ src/Solid.Practices.Composition/RuntimeAssemblyLoader.cs
@@ -6,8 +6,16 @@ using System.Runtime.Loader;
 
 namespace Solid.Practices.Composition
 {
+    /// <summary>
+    /// The runtime assembly loader.
+    /// </summary>
     public static class RuntimeAssemblyLoader
     {
+        /// <summary>
+        /// Returns the list os assemblies loaded from the respective files.
+        /// </summary>
+        /// <param name="files">The collection of files.</param>
+        /// <returns>The collection of assemblies.</returns>
         public static IEnumerable<Assembly> Get(IEnumerable<string> files)
         {
             return files.Select(r =>
