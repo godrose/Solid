d8c1cdcf8ce37eaee5b077ea42fe2b9d0617e3a4
Gennady Verdel <godrose@gmail.com>
2022-03-16 16:01:45 +0200

    chore: added safe for each method

--- src/Solid.Core/CollectionExtensions.cs
+++ src/Solid.Core/CollectionExtensions.cs
@@ -1,7 +1,29 @@
-﻿namespace Solid.Core
+﻿using System;
+using System.Collections.Generic;
+
+namespace Solid.Core
 {
-    internal static class CollectionExtensions
-    {         
+    /// <summary>
+    /// Extension methods for collections
+    /// </summary>
+    public static class CollectionExtensions
+    {   
+        /// <summary>
+        /// Invokes the passed action for each one of the non-null collection elements.
+        /// Gracefully returns for null collection case.
+        /// </summary>
+        /// <param name="collection">The collection.</param>
+        /// <param name="action">The action to be invoked.</param>
+        /// <typeparam name="T">The type of the element.</typeparam>
+        public static void ForEachSafe<T>(this IEnumerable<T> collection, Action<T> action)
+        {
+            if (collection == null) return;
+            foreach (var item in collection)
+            {
+                action(item);
+            }
+        }    
+        
         internal static string[] Patch(this string[] input)
         {
             return input == null || input.Length == 0 ? new[] { Consts.WildCard } : input;
